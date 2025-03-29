f10a6a4f7d8462f71348aad36431e9d902863c13
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2025-03-29 17:54:24 +0100

    Added DB context

---
+++ CloudDrive.Infrastructure/Repositories/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Entities = CloudDrive.Core.Domain.Entities;
+
+namespace CloudDrive.Infrastructure.Repositories
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Entities.User> Users { get; set; }
+        public DbSet<Entities.File> Files { get; set; }
+        public DbSet<Entities.FileVersion> FileVersions { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            
+        }
+    }
+}
