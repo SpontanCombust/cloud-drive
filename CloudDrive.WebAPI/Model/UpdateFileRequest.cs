87859fd24f42b4361a23255bc6965f4d8cf05e72
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2025-04-16 17:50:51 +0200

    Added endpoints for updating and deleting files

---
+++ CloudDrive.WebAPI/Model/UpdateFileRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace CloudDrive.WebAPI.Model
+{
+    public class UpdateFileRequest
+    {
+        public required IFormFile File { get; set; }
+        public required string ClientDirPath { get; set; }
+    }
+}
