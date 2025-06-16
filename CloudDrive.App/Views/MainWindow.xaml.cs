209649c24dd93586464f82dfa9e00ef0f053fb45
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2025-06-16 16:19:38 +0200

    Made the main window hide into system tray instead of closing immediately

--- CloudDrive.App/Views/MainWindow.xaml.cs
+++ CloudDrive.App/Views/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -45,6 +46,27 @@ namespace CloudDrive.App.Views
             {
                 MessageBox.Show("Wystąpił błąd podczas ładowania aplikacji: " + ex.Message);
             }
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            // Cancel the close and minimize to tray instead
+            e.Cancel = true;
+            this.WindowState = WindowState.Minimized;
+            this.ShowInTaskbar = false;
+        }
+
+        private void SystemTrayIconRestore_Click(object sender, RoutedEventArgs e)
+        {
+            this.ShowInTaskbar = true;
+            this.WindowState = WindowState.Normal;
+            this.Activate();
+        }
+
+        private void SystemTrayIconClose_Click(object sender, RoutedEventArgs e)
+        {
+            SystemTrayIcon.Visibility = Visibility.Collapsed;
+            Application.Current.Shutdown();
         }
     }
 }
