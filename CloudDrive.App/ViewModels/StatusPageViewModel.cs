b9fe9fa0bdfa659ac9b3c9f02a530972b795f59b
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2025-06-18 00:02:47 +0200

    Added option to print debug logs on the status page

---
+++ CloudDrive.App/ViewModels/StatusPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudDrive.App.ViewModels
+{
+    public class StatusPageViewModel : ViewModel
+    {
+        public bool EnableDebugLogs { get; set; } = false;
+    }
+}
