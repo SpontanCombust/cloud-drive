6ade0fe9144bf89a3c89b0749fa35639c24ad692
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2025-06-18 02:32:07 +0200

    Reworked StatusPage using MVVM toolkit, removed ViewModel class

--- CloudDrive.App/ViewModels/StatusPageViewModel.cs
+++ CloudDrive.App/ViewModels/StatusPageViewModel.cs
@@ -1,13 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows;
 
 namespace CloudDrive.App.ViewModels
 {
-    public class StatusPageViewModel : ViewModel
+    public partial class StatusPageViewModel : ObservableObject
     {
-        public bool EnableDebugLogs { get; set; } = false;
+        [ObservableProperty]
+        private string logs;
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(LogsTextWrapping))]
+        private bool logsTextWrappingEnabled; 
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(AllowManualFullSync))]
+        [NotifyPropertyChangedFor(nameof(SyncInProgressSpinnerVisibility))]
+        private bool syncIsInProgress;
+
+        [ObservableProperty]
+        private bool debugLogsEnabled;
+         
+
+        public TextWrapping LogsTextWrapping =>
+            LogsTextWrappingEnabled ? TextWrapping.Wrap : TextWrapping.NoWrap;
+
+        public bool AllowManualFullSync => !SyncIsInProgress;
+
+        public Visibility SyncInProgressSpinnerVisibility =>
+            SyncIsInProgress ? Visibility.Visible : Visibility.Collapsed;
+
+
+        public StatusPageViewModel()
+        {
+            logs = string.Empty;
+            logsTextWrappingEnabled = true;
+            syncIsInProgress = false;
+            debugLogsEnabled = false;
+        }
     }
 }
