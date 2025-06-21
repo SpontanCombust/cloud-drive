1ffe99abe1b2feee4bc230d3ed1375c6fc425e58
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2025-06-21 15:35:45 +0200

    fix staging helper modification detection

--- CloudDrive.App/Utils/FileIndexStagingHelper.cs
+++ CloudDrive.App/Utils/FileIndexStagingHelper.cs
@@ -3,27 +3,29 @@
 namespace CloudDrive.App.Utils
 {
     /// <summary>
-    /// Class designed to deduce the changes in the file index between the local and remote state.
+    /// Klasa stworzona do wykrywania zmian w indeksie plików pomiędzy trzema stanami: lokalnym utwalonym, lokalnym "na żywo" i obecnym zdalnym.
+    /// Istnieje dla potrzeb pełnej synchronizacji i pobierania zmian z serwera.
+    /// Nie jest w stanie wykryć złożonych zmian lokalnych typu przeniesienie pliku, jeśli ta operacja została wykonana poza czasem pracy serwisu FileSystemSyncWatcher.
     /// </summary>
     public class FileIndexStagingHelper
     {
-        private HashSet<WatchedFileSystemPath> _localCommitedPaths;
-        private HashSet<WatchedFileSystemPath> _localIncomingPaths;
-        private HashSet<WatchedFileSystemPath> _remoteIncomingPaths;
+        // Lokalne operacje dodawania i usuwania plików wykrywane są na podstawie istnienia ich ścieżek w indeksie plików...
+        private Dictionary<WatchedFileSystemPath, LocalCommitedFileIndexEntry> _localCommitedPathDict = new();
+        private Dictionary<WatchedFileSystemPath, LocalIncomingFileIndexEntry> _localIncomingPathDict = new();
+        private HashSet<WatchedFileSystemPath> _localCommitedPaths = new();
+        private HashSet<WatchedFileSystemPath> _localIncomingPaths = new();
 
-        private Dictionary<WatchedFileSystemPath, LocalCommitedFileIndexEntry> _localCommitedDict;
-        private Dictionary<WatchedFileSystemPath, LocalIncomingFileIndexEntry> _localIncomingDict;
-        private Dictionary<WatchedFileSystemPath, RemoteIncomingFileIndexEntry> _remoteIncomingDict;
+        // Operacje na plikach, które zostały już wysłane na serwer, zatwierdzone i przypisane zostało im ID są wykrywane natomiast na podstawie ID plików w indeksie plików.
+        // Jest tak dlatego, że zmiana ścieżki pliku nie wpływa na jego ID, ale jak najbardziej wpływa na jego ścieżkę w indeksie plików.
+        private Dictionary<Guid, LocalCommitedFileIndexEntry> _localCommitedIdDict = new();
+        private Dictionary<Guid, RemoteIncomingFileIndexEntry> _remoteIncomingIdDict = new();
+        private HashSet<Guid> _localCommitedIds = new();
+        private HashSet<Guid> _remoteIncomingIds = new();
+
 
         public FileIndexStagingHelper()
         {
-            _localCommitedPaths = new();
-            _localIncomingPaths = new();
-            _remoteIncomingPaths = new();
-
-            _localCommitedDict = new();
-            _localIncomingDict = new();
-            _remoteIncomingDict = new();
+            
         }
 
         public void Stage(
@@ -31,28 +33,37 @@ namespace CloudDrive.App.Utils
             IEnumerable<LocalIncomingFileIndexEntry> localIncomingEntries,
             IEnumerable<RemoteIncomingFileIndexEntry> remoteIncomingEntries)
         {
-            _localCommitedDict = localCommitedEntries.ToDictionary(
+            _localCommitedPathDict = localCommitedEntries.ToDictionary(
                 e => new WatchedFileSystemPath(e.FullPath, e.WatchedFolderPath, e.IsDirectory), 
                 e => e);
-            _localIncomingDict = localIncomingEntries.ToDictionary(
-                e => new WatchedFileSystemPath(e.FullPath, e.WatchedFolderPath, e.IsDirectory), 
-                e => e);
-            _remoteIncomingDict = remoteIncomingEntries.ToDictionary(
+            _localIncomingPathDict = localIncomingEntries.ToDictionary(
                 e => new WatchedFileSystemPath(e.FullPath, e.WatchedFolderPath, e.IsDirectory), 
                 e => e);
 
-            _localCommitedPaths = _localCommitedDict.Keys.ToHashSet();
-            _localIncomingPaths = _localIncomingDict.Keys.ToHashSet();
-            _remoteIncomingPaths = _remoteIncomingDict.Keys.ToHashSet();
+            _localCommitedPaths = _localCommitedPathDict.Keys.ToHashSet();
+            _localIncomingPaths = _localIncomingPathDict.Keys.ToHashSet();
+
+
+            _localCommitedIdDict = localCommitedEntries.ToDictionary(
+                e => e.FileId, 
+                e => e);
+            _remoteIncomingIdDict = remoteIncomingEntries.ToDictionary(
+                e => e.FileId, 
+                e => e);
+
+            _localCommitedIds = _localCommitedIdDict.Keys.ToHashSet();
+            _remoteIncomingIds = _remoteIncomingIdDict.Keys.ToHashSet();
         }
 
         public IEnumerable<FileIndexEntryStagingSet.AddedLocally> AddedLocally()
         {
-            var paths = _localIncomingPaths.Except(_localCommitedPaths);//.Except(_remoteIncomingPaths);
+            // tutaj nowy plik mógłby być tak na prawdę plikiem przeniesionym z innej lokalizacji, ale nie mamy informacji o tym, że był przeniesiony
+            // pozyskanie tej informacji wymaga analizy heurystycznej systemu plików
+            var paths = _localIncomingPaths.Except(_localCommitedPaths);
 
             foreach (var path in paths)
             {
-                if (_localIncomingDict.TryGetValue(path, out var localIncomingEntry))
+                if (_localIncomingPathDict.TryGetValue(path, out var localIncomingEntry))
                 {
                     yield return new FileIndexEntryStagingSet.AddedLocally
                     {
@@ -64,11 +75,11 @@ namespace CloudDrive.App.Utils
 
         public IEnumerable<FileIndexEntryStagingSet.AddedRemotely> AddedRemotely()
         {
-            var paths = _remoteIncomingPaths.Except(_localCommitedPaths);//.Except(_localIncomingPaths);
+            var fileIds = _remoteIncomingIds.Except(_localCommitedIds);
 
-            foreach (var path in paths)
+            foreach (var fileId in fileIds)
             {
-                if (_remoteIncomingDict.TryGetValue(path, out var remoteIncomingEntry))
+                if (_remoteIncomingIdDict.TryGetValue(fileId, out var remoteIncomingEntry))
                 {
                     yield return new FileIndexEntryStagingSet.AddedRemotely
                     {
@@ -80,13 +91,15 @@ namespace CloudDrive.App.Utils
 
         public IEnumerable<FileIndexEntryStagingSet.DeletedLocally> DeletedLocally()
         {
-            var paths = _localCommitedPaths.Except(_localIncomingPaths);//.Intersect(_remoteIncomingPaths);
+            // tutaj usunięcie pliku mógłoby być tak na prawdę efektem przeniesienia pliku wciąż istniejącego, ale nie mamy informacji o tym, że był przeniesiony
+            // pozyskanie tej informacji wymaga analizy heurystycznej systemu plików
+            var paths = _localCommitedPaths.Except(_localIncomingPaths);
 
             foreach (var path in paths)
             {
-                if (_localCommitedDict.TryGetValue(path, out var localCommitedEntry))
+                if (_localCommitedPathDict.TryGetValue(path, out var localCommitedEntry))
                 {
-                    _remoteIncomingDict.TryGetValue(path, out var remoteIncomingEntry);
+                    _remoteIncomingIdDict.TryGetValue(localCommitedEntry.FileId, out var remoteIncomingEntry);
 
                     yield return new FileIndexEntryStagingSet.DeletedLocally
                     {
@@ -99,13 +112,13 @@ namespace CloudDrive.App.Utils
 
         public IEnumerable<FileIndexEntryStagingSet.DeletedRemotely> DeletedRemotely()
         {
-            var paths = _localCommitedPaths.Except(_remoteIncomingPaths);//.Intersect(_localIncomingPaths);
+            var fileIds = _localCommitedIds.Except(_remoteIncomingIds);
 
-            foreach (var path in paths)
+            foreach (var fileId in fileIds)
             {
-                if (_localCommitedDict.TryGetValue(path, out var localCommitedEntry))
+                if (_localCommitedIdDict.TryGetValue(fileId, out var localCommitedEntry))
                 {
-                    _localIncomingDict.TryGetValue(path, out var localIncomingEntry);
+                    _localIncomingPathDict.TryGetValue(localCommitedEntry.GetWatchedFileSystemPath(), out var localIncomingEntry);
 
                     yield return new FileIndexEntryStagingSet.DeletedRemotely
                     {
@@ -118,15 +131,17 @@ namespace CloudDrive.App.Utils
 
         public IEnumerable<FileIndexEntryStagingSet.ModifedLocally> ModifiedLocally()
         {
-            var paths = _localCommitedPaths.Intersect(_localIncomingPaths);//.Intersect(_remoteIncomingPaths);
+            // tutaj może być wykryta tylko zmiana zawartości pliku
+            // wykrycie przeniesienia pliku wymaga bardziej złożonej analizy heurystycznej systemu plików
+            var paths = _localCommitedPaths.Intersect(_localIncomingPaths);
 
             foreach (var path in paths)
             {
-                if (_localCommitedDict.TryGetValue(path, out var localCommitedEntry) &&
-                    _localIncomingDict.TryGetValue(path, out var localIncomingEntry) &&
+                if (_localCommitedPathDict.TryGetValue(path, out var localCommitedEntry) &&
+                    _localIncomingPathDict.TryGetValue(path, out var localIncomingEntry) &&
                     localIncomingEntry.LastModifiedDate > localCommitedEntry.CommitedDate)
                 {
-                    _remoteIncomingDict.TryGetValue(path, out var remoteIncomingEntry);
+                    _remoteIncomingIdDict.TryGetValue(localCommitedEntry.FileId, out var remoteIncomingEntry);
 
                     yield return new FileIndexEntryStagingSet.ModifedLocally
                     {
@@ -140,15 +155,15 @@ namespace CloudDrive.App.Utils
 
         public IEnumerable<FileIndexEntryStagingSet.ModifiedRemotely> ModifiedRemotely()
         {
-            var paths = _localCommitedPaths.Intersect(_remoteIncomingPaths);//.Intersect(_localIncomingPaths);
+            var fileIds = _localCommitedIds.Intersect(_remoteIncomingIds);
 
-            foreach (var path in paths)
+            foreach (var fileId in fileIds)
             {
-                if (_localCommitedDict.TryGetValue(path, out var localCommitedEntry) &&
-                    _remoteIncomingDict.TryGetValue(path, out var remoteIncomingEntry) &&
+                if (_localCommitedIdDict.TryGetValue(fileId, out var localCommitedEntry) &&
+                    _remoteIncomingIdDict.TryGetValue(fileId, out var remoteIncomingEntry) &&
                     remoteIncomingEntry.VersionCreatedDate > localCommitedEntry.CommitedDate)
                 {
-                    _localIncomingDict.TryGetValue(path, out var localIncomingEntry);
+                    _localIncomingPathDict.TryGetValue(localCommitedEntry.GetWatchedFileSystemPath(), out var localIncomingEntry);
 
                     yield return new FileIndexEntryStagingSet.ModifiedRemotely
                     {
